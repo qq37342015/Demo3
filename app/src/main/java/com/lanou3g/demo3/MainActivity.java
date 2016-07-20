edb6c15f3bed626526cbe27433436752a1cc9cd3
chenluyao <373420159@qq.com>
2016-07-20 17:30:37 +0800

    Initial commit cccc

--- app/src/main/java/com/lanou3g/demo3/MainActivity.java
+++ app/src/main/java/com/lanou3g/demo3/MainActivity.java
@@ -1,13 +1,22 @@
 package com.lanou3g.demo3;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
